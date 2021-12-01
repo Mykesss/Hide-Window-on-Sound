e86db97edba2cbfef28b7e2b4bbbd8e07e25d122
mykes <mykes1777@gmail.com>
2021-12-02 03:01:13 +0800

    god remains dead

--- closeOnSound.py
+++ closeOnSound.py
@@ -3,6 +3,7 @@ import numpy as np
 import win32gui, win32con
 import os
 import keyboard
+from sound import Sound
 
 def windowEnumHandler(hwnd, resultList):
     if win32gui.IsWindowVisible(hwnd) and win32gui.GetWindowText(hwnd) != '':
@@ -25,7 +26,15 @@ def printSound(indata, outdata, frames, time, status):
     volume = int(volume_norm)
     if(volume > volumeThreshold):
         print('Volume level:',volume)
-        minimizeWindow()
+        try:
+            Sound.mute()
+            minimizeWindow()
+            print("\tPaused. F4 to resume")
+            keyboard.wait('F4')
+            print("\tResuming")
+            Sound.volume_up()
+        except: 
+            pass
 
 volumeThreshold = 20
 applicationName = input("Enter application name(must be open): ").lower()
@@ -36,7 +45,7 @@ print("\t\tListening.......")
 print("You can now minimize this window or close to quit")
 print("Press any key to pause/resume\n")
 openWindows = getAppList()
-paused = False
+paused = False 
 for i in openWindows:
     if applicationName in i[1].lower():
         windowHandle = i[0]
@@ -45,16 +54,17 @@ for i in openWindows:
 with sd.Stream(callback=printSound):
     while(True):
         focusedWindow = win32gui.GetWindowText(win32gui.GetForegroundWindow())
-        if keyboard.read_key() and paused == False: #hating this function with passion
+        if keyboard.is_pressed('F4') and paused == False: #hating this function with passion
             focusedWindow = win32gui.GetWindowText(win32gui.GetForegroundWindow())
             if "py.exe" in focusedWindow:
                 volumeThreshold = 10000
                 paused = True
-                print("\tPaused")
-        elif keyboard.read_key()and paused == True:
+                print("\Paused. F4 to resume")
+        elif keyboard.is_pressed('F4') and paused == True:
             focusedWindow = win32gui.GetWindowText(win32gui.GetForegroundWindow())
             if "py.exe" in focusedWindow:
                 volumeThreshold = userSavedVolume
                 paused = False
                 print("\tResuming")
+                Sound.volume_up()
         sd.sleep(500)
